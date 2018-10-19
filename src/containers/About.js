b403793a739263fb58dc557d949b787fd180fa2a
taze-fullstack <taze@fullstack.ph>
2018-10-19 14:40:10 +0800

    try history

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import { Prefetch, Link } from 'react-static'
+import createHistory from 'history/createBrowserHistory'
 //
 const { GREETING } = process.env
+const history = createHistory()
 export default class About extends React.Component {
-  render() {
+  render() {  
     return (
       <div>
         <h1>This is what we're all about.</h1>
         <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
         <div>{GREETING}</div>
+        <Prefetch path='/blog'>
+          <Link to="/blog" onClick={
+            (e) => {
+              e.preventDefault()
+              history.push('/blog')
+            }
+          }>
+            Try Prefetch Blog
+          </Link>
+        </Prefetch>
       </div>    
     )
   }
